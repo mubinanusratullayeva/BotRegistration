d0741d4444774bd785ae4c89c61992e6ffd1572d
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-02-14 16:54:17 +0500

    created register states

--- handlers/reg_handlers.py
+++ handlers/reg_handlers.py
@@ -1,3 +1,10 @@
 from aiogram import Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, ReplyKeyboardRemove
+
+from config import DB_NAME
+from keyboards.reg_keyboards import kb_request_contact
+from states.reg_states import RegisterStates
+
 
 reg_router = Router()
